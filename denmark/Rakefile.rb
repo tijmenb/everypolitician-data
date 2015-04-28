800582c9a77076104c3f78ecce27077a8adce8f4
Tony Bowden <tony@tmtm.com>
2015-04-28 22:13:39 +0100

    Remove people who don't have `currentMP` set

--- denmark/Rakefile.rb
+++ denmark/Rakefile.rb
@@ -5,7 +5,15 @@ require_relative '../rakefile_common.rb'
 
 @DEST = 'denmark'
 
+task :dk_remove_not_current => :load_json do
+  @json[:persons].keep_if { |p| p[:data] && p[:data][:currentMP] && p[:data][:currentMP].first == 'y' }
+end
+
+task :clean_orphaned_memberships => :dk_remove_not_current
+
 task :process_json => [
+  :dk_remove_not_current,
+  :clean_orphaned_memberships,
   :ensure_legislative_period,
   :switch_party_to_behalf,
   :default_memberships_to_current_term
