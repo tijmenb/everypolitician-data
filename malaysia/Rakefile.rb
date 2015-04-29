6fcb36ba88d4ed0ec5e68dc5a95457a40dc1e1b0
Tony Bowden <tony@tmtm.com>
2015-04-29 09:01:04 +0100

    WIP: Match Parliament Representative roles

--- malaysia/Rakefile.rb
+++ malaysia/Rakefile.rb
@@ -34,6 +34,16 @@ task :downcase_classifications => :my_fixup_orgs do
   @json[:organizations].each { |o| o[:classification] = o[:classification].to_s.downcase } 
 end
 
+task :clean_orphaned_memberships => :clean_memberships_to_missing_posts
+
+task :clean_memberships_to_missing_posts => :load_json do
+  all_post_ids = @json[:posts].map { |post| post[:id] }
+  # @json[:memberships].keep_if { |m| m[:post_id].nil? or all_post_ids.include? m[:post_id] }
+  @json[:memberships].each { |m| 
+    next if m[:post_id].nil?
+   raise "Orphaned" unless all_post_ids.include? m[:post_id] 
+  }
+end
 
 task :clean_orphaned_people => :clean_orphaned_memberships do
   keep_people = @json[:memberships].map { |m| m[:person_id] }
@@ -52,6 +62,26 @@ task :remove_unwanted_orgs => :downcase_classifications do
   @json[:organizations].keep_if { |m| keep_orgs.include? m[:id] }
 end
 
+task :my_fixup_behalfs => :clean_orphaned_people do
+  # Every Member of Parliament has an associated Parliament Representative — merge these
+
+  @json[:memberships].find_all { |o| o[:role] == 'Parliament Representative' }.each do |rep|
+    matching = @json[:memberships].find_all { |m| 
+      m[:person_id] == rep[:person_id] and
+      # m[:post_id] == rep[:post_id] and
+      m[:start_date] == rep[:start_date] and
+      m[:end_date] == rep[:end_date] and
+      m[:role] == 'Member of Parliament' 
+    } or raise "No matching MP membership for #{rep}" 
+    require 'pry'
+    binding.pry
+    raise "Too many MP memberships for #{rep}: #{matching}" if matching.count > 1
+    
+  end
+
+end
+
+
 
 task :process_json => [
   :my_fixup_orgs,
@@ -60,6 +90,7 @@ task :process_json => [
   :remove_candidates,
   :clean_orphaned_memberships, 
   :clean_orphaned_people,
+  :my_fixup_behalfs,
   :ensure_legislative_period,
   :default_memberships_to_current_term
 ] 
