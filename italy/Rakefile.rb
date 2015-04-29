e4ec3263a505b569c6d347a03880b08450a2e99c
Tony Bowden <tony@tmtm.com>
2015-04-29 13:44:21 +0100

    Supply current_term information for Italy

--- italy/Rakefile.rb
+++ italy/Rakefile.rb
@@ -3,4 +3,10 @@ require_relative '../rakefile_common.rb'
 @POPIT = 'parlamento-test'
 @DEST = 'italy'
 
+@current_term = { 
+  id: 'legislatura/17',
+  name: 'XVII Legislatura',
+  start_date: '2013-03-15'
+}
+
 task :process_json => :default_memberships_to_current_term
