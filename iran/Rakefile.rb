e42c401d6674d8651461bd210a9d1c195a93fa6d
Tony Bowden <tony@tmtm.com>
2015-04-30 22:33:49 +0100

    Add Iran

---
+++ iran/Rakefile.rb
@@ -0,0 +1,12 @@
+require_relative '../rakefile_common.rb'
+
+@POPIT = 'iran-test'
+@DEST = 'iran'
+
+@current_term = { 
+  id: "term/9",
+  name: "9th Assembly",
+  start_date: "2012-05-27",
+}
+
+task :process_json => :default_memberships_to_current_term
