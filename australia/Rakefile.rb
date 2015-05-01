5c2670404bcc526a62870d7846e6e8903efeb0b3
Tony Bowden <tony@tmtm.com>
2015-05-01 18:38:19 +0100

    Australia: use `@current_term`

--- australia/Rakefile.rb
+++ australia/Rakefile.rb
@@ -4,6 +4,11 @@ require_relative '../rakefile_common.rb'
 @DEST = 'australia'
 @POPIT = 'australia-test'
 
+@current_term = { 
+  id: 'term/44',
+  name: '44th Parliament',
+  start_date: '2013-09-07',
+}
 
 task :connect_chambers => :ensure_legislature_exists do
   better_name = { 
@@ -16,32 +21,5 @@ task :connect_chambers => :ensure_legislature_exists do
   end
 end
 
-task :add_legislative_period => :ensure_legislature_exists do
-  # Don't use the default one, because we have info
-  # http://en.wikipedia.org/wiki/Chronology_of_Australian_federal_parliaments
-  # Let's simply deal with Parliaments, ignoring the 6-year Senate
-
-  leg = @json[:organizations].find { |h| h[:classification] == 'legislature' } or raise "No legislature"
-  unless leg.has_key?(:legislative_periods) and not leg[:legislative_periods].count.zero? 
-    leg[:legislative_periods] = [{
-      id: 'term/44',
-      name: '44th Parliament',
-      start_date: '2013-09-07',
-      classification: 'legislative period',
-    }]
-  end
-
-  chambers = @json[:organizations].find_all { |h| h[:classification] == 'chamber' } or raise "No chambers"
-  chamber_ids = chambers.map { |c| c[:id] }
-  @json[:memberships].find_all { |m| m[:role] == 'member' && chamber_ids.include?(m[:organization_id]) }.each do |m|
-    m[:legislative_period_id] ||= 'term/44'
-  end
-  
-end
-
-task :process_json => [
-  :load_json, 
-  :connect_chambers,
-  :add_legislative_period,
-] 
+task :process_json => [:connect_chambers, :default_memberships_to_current_term]
 
