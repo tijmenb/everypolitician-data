8e4fb6262aa084e295531b5566aec9e0ebe5a923
Tony Bowden <tony@tmtm.com>
2015-05-02 17:38:51 +0100

    pull data from Morph-scraped version instead

--- chile/Rakefile.rb
+++ chile/Rakefile.rb
@@ -1,20 +1,4 @@
-require_relative '../rakefile_popit.rb'
+require_relative '../rakefile_morph.rb'
 
-@POPIT = 'pmocl'
+@MORPH = 'tmtmtmtm/chile-opendata'
 @DEST = 'chile'
-
-@current_term = { 
-  id: "term/2014",
-  name: "Legislativo 2014-2018",
-  start_date: "2014-03-11",
-  end_date: "2018-03-10",
-}
-
-# Must be done in correct order
-task :default_memberships_to_current_term => :switch_party_to_behalf
-
-task :process_json => [
-  :clean_orphaned_memberships, 
-  :ensure_legislative_period,
-  :default_memberships_to_current_term
-] 
