d3787a349b35f74bf4fc678e98677fb79c9d0aaf
Tony Bowden <tony@tmtm.com>
2015-05-03 16:55:58 +0100

    Refresh with new data from 25321d9

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,3 +1,4 @@
+require 'tmpdir'
 
 @COUNTRIES = FileList['*/Rakefile.rb'].pathmap('%d')
 
@@ -25,8 +26,24 @@ end
 
 desc "Publish data"
 task :publish do
-  @COUNTRIES.each do |country| 
-    cp "#{country}/final.json", "/Users/tony/Code/popolo-viewer-sinatra/data/#{country}.json"
+  Dir.mktmpdir do |dir|
+    cwd = Dir.pwd
+    puts "Currently in #{cwd}"
+    puts "cd #{dir}"
+    last_commit = %x{ git rev-parse --short HEAD }.chomp
+    branch_name = "epdata-#{last_commit}"
+
+    %x[ hub clone mysociety/popolo-viewer-sinatra #{dir} ]
+    %x[ git checkout -b epdata-#{last_commit} ]
+    @COUNTRIES.each do |country| 
+      cp "#{country}/final.json", "#{dir}/data/#{country}.json"
+    end
+    %x[ git add . ]
+    %x[ git commit -m "Refresh with new data from #{last_commit}" ]
+    %x[ git push -u origin epdata-a28fc5f ]
+    %x[ hub pull-request -m "Refresh with new data from #{last_commit}" ]
+    require 'pry'
+    binding.pry
   end
 end
 
