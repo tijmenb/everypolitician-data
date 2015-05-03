13292e8193062a5cb14eb51cae7be57fbc51d84b
Tony Bowden <tony@tmtm.com>
2015-05-03 14:27:32 +0100

    Add Kazakhstan

---
+++ kazakhstan/Rakefile.rb
@@ -0,0 +1,12 @@
+require_relative '../rakefile_morph.rb'
+
+@DEST = 'kazakhstan'
+@MORPH = 'tmtmtmtm/kazakh-mazhilis'
+
+# http://www.parlam.kz/en/mazhilis/history?id=history
+@current_term = { 
+  id: 'term/5',
+  name: 'Fifth Convocation',
+  start_date: '2012-01-15',
+}
+
