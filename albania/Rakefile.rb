cef082d89989374354978b0a8106e1cbfb06a141
Tony Bowden <tony@tmtm.com>
2015-05-03 21:23:41 +0100

    refresh data

--- albania/Rakefile.rb
+++ albania/Rakefile.rb
@@ -14,6 +14,18 @@ task :ensure_legislative_period => :cleanup
 task :cleanup => :ensure_legislature_exists do
   @json[:organizations].delete_if { |o| o[:classification] == 'committee' }
 
+  # leg[:legislative_periods], @orgs[:organizations] = @orgs[:organizations].partition { |o| o[:classification] == 'chamber' }
+  terms, orgs = @json[:organizations].partition { |o| o[:classification] == 'chamber' }
+  require 'pry'
+  binding.pry
+
+  @json[:organizations] = orgs
   leg = @json[:organizations].find { |h| h[:classification] == 'legislature' } or raise "No legislature"
-  leg[:legislative_periods], @orgs[:organizations] = @orgs[:organizations].partition { |o| o[:classification] == 'chamber' }
+  leg[:legislative_periods] = terms.map do |t|
+    t[:classification] == 'legislative_period_id'
+    t[:start_date] = t.delete :founding_date
+    t[:end_date] = t.delete :dissolution_date
+    t
+  end
+
 end
