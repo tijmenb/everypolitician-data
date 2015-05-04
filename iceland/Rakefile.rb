e3be0611c32f96ea6b45c72633f45c751becd8ce
Tony Bowden <tony@tmtm.com>
2015-05-04 15:43:58 +0100

    Refresh Iceland data
    The Morph scraper changed the names of the Terms

--- iceland/Rakefile.rb
+++ iceland/Rakefile.rb
@@ -9,10 +9,10 @@ require_relative '../rakefile_morph.rb'
 task 'final.json' => :add_term_dates
 task :add_term_dates => :ensure_legislative_period do
   parl = @json[:organizations].find { |h| h[:classification] == 'legislature' } or raise "No legislature"
-  parl[:legislative_periods].each do |t|
-    (start_date, end_date) = t[:name].split(/\D+/)
-    t[:start_date] = start_date if start_date
-    t[:end_date] = end_date if end_date
+  terms = parl[:legislative_periods].sort_by { |t| t[:name] }
+  terms.each_with_index do |t, i|
+    t[:start_date] = t[:name].clone
+    t[:end_date] = terms[i+1][:name].clone unless i+1 == terms.size
     t[:name].prepend 'Alþingi '
   end
 end
