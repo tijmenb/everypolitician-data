2093fdf407beef11a92dfc8a9bbe8d88dfbe96d7
Tony Bowden <tony@tmtm.com>
2015-05-05 09:18:16 +0100

    Add names for Parliament

--- turkey/Rakefile.rb
+++ turkey/Rakefile.rb
@@ -6,4 +6,38 @@ require_relative '../rakefile_morph.rb'
   name: 'Grand National Assembly'
 }
 
+@TERMS = [
+  { 
+    id: 'term/24',
+    name: '24th Parliament',
+  },
+  { 
+    id: 'term/23',
+    name: '23rd Parliament',
+  },
+  { 
+    id: 'term/22',
+    name: '22nd Parliament',
+  },
+  { 
+    id: 'term/21',
+    name: '21st Parliament',
+  },
+  { 
+    id: 'term/20',
+    name: '20th Parliament',
+  },
+
+]
+
+task 'final.json' => :add_term_dates
+task :add_term_dates => :ensure_legislative_period do
+  parl = @json[:organizations].find { |h| h[:classification] == 'legislature' } or raise "No legislature"
+  parl[:legislative_periods].each do |t|
+    warn "Term #{t}"
+    t.merge! @TERMS.find { |termdata| termdata[:id] == t[:id] }
+    warn "\tNow #{t}"
+  end
+end
+
 
