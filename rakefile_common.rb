bcc2cbac3144d7d842061aeea89a94a594744b8a
Tony Bowden <tony@tmtm.com>
2015-05-06 23:31:33 +0100

    New style of Rakefile

--- rakefile_common.rb
+++ rakefile_common.rb
@@ -3,86 +3,134 @@ require 'open-uri'
 require 'rake/clean'
 require 'pry'
 
-CLEAN.include('final.json')
 
 Numeric.class_eval { def empty?; false; end }
 
-task :rebuild => [ :clean, 'final.json' ]
-
+desc "Rebuild from source data"
+task :rebuild => [ :clobber, 'final.json' ]
 task :default => 'final.json'
 
-task :load_json => 'clean.json' do
-  @json = JSON.parse(File.read('clean.json'), symbolize_names: true )
-end
+desc "Remove unwanted data from source"
+task :whittle => [:clobber, 'clean.json']
 
-task :process_json => :ensure_memberships_have_term
+namespace :whittle do
 
-file 'final.json' => :process_json do
-  File.write('final.json', JSON.pretty_generate(@json))
-end  
+  # Source-specific files must provide a whittle:load
+
+  file 'clean.json' => :write 
+  CLOBBER.include('clean.json')
+
+  task :write => :load do
+    File.write('clean.json', JSON.pretty_generate(@json))
+  end
 
 
-
-
-task :clean_orphaned_memberships => :load_json do
-  @json[:memberships].keep_if { |m|
-    @json[:organizations].find { |o| o[:id] == m[:organization_id] } and
-    @json[:persons].find { |p| p[:id] == m[:person_id] } 
-  }
-end  
-
-task :ensure_legislature_exists => :load_json do
-  if @json[:organizations].find_all { |h| h[:classification] == 'legislature' }.count.zero?
-    @json[:organizations] << {
-      classification: "legislature",
-      name: "Legislature",
-      id: "legislature",
+  #---------------------------------------------------------------------
+  # Rule: No orphaned memberships
+  #---------------------------------------------------------------------
+  task :write => :no_orphaned_memberships
+  task :no_orphaned_memberships => :load do
+    @json[:memberships].keep_if { |m|
+      @json[:organizations].find { |o| o[:id] == m[:organization_id] } and
+      @json[:persons].find { |p| p[:id] == m[:person_id] } 
     }
+  end  
+end
+
+
+namespace :transform do
+
+  file 'final.json' => :write
+  CLOBBER.include('final.json')
+
+  task :load => 'clean.json' do
+    @json = JSON.parse(File.read('clean.json'), symbolize_names: true )
   end
-end
 
-task :name_legislature => :ensure_legislature_exists do
-  if (@LEGISLATURE)
-    leg = @json[:organizations].find_all { |h| h[:classification] == 'legislature' }
-    raise "More than one legislature exists, and @LEGISLATURE set" if leg.count > 1
-    leg.first.merge! @LEGISLATURE
-  end
-end
+  task :write do
+    File.write('final.json', JSON.pretty_generate(@json))
+  end  
 
-task :build_term_info do
-  @_default_term = {
-    id: 'term/current',
-    name: 'current',
-    classification: 'legislative period',
-  }
-  @_default_term.merge! @current_term if @current_term
-end
-
-task :ensure_legislative_period => [ :name_legislature, :build_term_info ] do
-  leg = @json[:organizations].find { |h| h[:classification] == 'legislature' } or raise "No legislature"
-  unless leg.has_key?(:legislative_periods) and not leg[:legislative_periods].count.zero? 
-    # TODO extend this also allow simple provision of historic terms
-    leg[:legislative_periods] = [ @_default_term ]
-  end
-end
-
-task :switch_party_to_behalf => :ensure_legislature_exists do
-  # TODO: Only works if *all* other Orgs are Parties
-  # TODO: Only works for unicameral legislature
-  @json[:organizations].find_all { |h| h[:classification] != 'legislature' }.each do |o|
-    @json[:memberships].find_all { |m| m[:organization_id] == o[:id] }.each do |m|
-      m[:role] = 'member'
-      m[:on_behalf_of_id] = o[:id]
-      m[:organization_id] = 'legislature'
+  #---------------------------------------------------------------------
+  # Rule: There must be a legislature
+  #---------------------------------------------------------------------
+  task :write => :ensure_legislature
+  task :ensure_legislature => :load do
+    if @json[:organizations].find_all { |h| h[:classification] == 'legislature' }.count.zero?
+      @json[:organizations] << {
+        classification: "legislature",
+        name: "Legislature",
+        id: "legislature",
+      }
     end
   end
-end
 
-task :ensure_memberships_have_term => :ensure_legislative_period do
-  leg_ids = @json[:organizations].find_all { |o| %w(legislature chamber).include? o[:classification] }.map { |o| o[:id] }
-  @json[:memberships].find_all { |m| m[:role] == 'member' and leg_ids.include? m[:organization_id] }.each do |m|
-    m[:legislative_period_id] ||= @_default_term[:id] 
+  #---------------------------------------------------------------------
+  # Rule: The legislature must be named
+  #---------------------------------------------------------------------
+  task :write => :name_legislature
+  task :name_legislature => :ensure_legislature do
+    # Can be defined in country-level rakefile
+    if (@LEGISLATURE)
+      leg = @json[:organizations].find_all { |h| h[:classification] == 'legislature' }
+      raise "More than one legislature exists, and @LEGISLATURE set" if leg.count > 1
+      leg.first.merge! @LEGISLATURE
+    end
   end
+
+  #---------------------------------------------------------------------
+  # Rule: There must be at least one term
+  #---------------------------------------------------------------------
+  task :write => :ensure_term
+
+  def default_term 
+    return @_default_term if @_default_term
+    @_default_term = {
+      id: 'term/current',
+      name: 'current',
+      classification: 'legislative period',
+    }
+    # Merge in anything defined by the individual country
+    @_default_term.merge! @current_term if @current_term
+    @_default_term
+  end
+
+  task :ensure_term => :ensure_legislature do
+    leg = @json[:organizations].find { |h| h[:classification] == 'legislature' } or raise "No legislature"
+    unless leg.has_key?(:legislative_periods) and not leg[:legislative_periods].count.zero? 
+      # TODO extend this also allow simple provision of historic terms
+      leg[:legislative_periods] = [ default_term ]
+    end
+  end
+
+  #---------------------------------------------------------------------
+  # Rule: Legislative Memberships must be for a Term
+  #---------------------------------------------------------------------
+  task :write => :ensure_membership_terms
+  task :ensure_membership_terms => :ensure_term do
+    leg_ids = @json[:organizations].find_all { |o| %w(legislature chamber).include? o[:classification] }.map { |o| o[:id] }
+    @json[:memberships].find_all { |m| m[:role] == 'member' and leg_ids.include? m[:organization_id] }.each do |m|
+      m[:legislative_period_id] ||= default_term[:id] 
+    end
+  end
+
+  #---------------------------------------------------------------------
+  # Rule: Legislative Memberships must have `on_behalf_of`
+  #---------------------------------------------------------------------
+  # This version only works if the only other Orgs are the Parties and
+  # only for unicameral legislatures, so it's not enabled by default.
+  # Trigger it if those are true, or hook in something better.
+  # task :write => :ensure_behalf_of
+  task :ensure_behalf_of => :ensure_legislature do
+    @json[:organizations].find_all { |h| h[:classification] != 'legislature' }.each do |o|
+      @json[:memberships].find_all { |m| m[:organization_id] == o[:id] }.each do |m|
+        m[:role] = 'member'
+        m[:on_behalf_of_id] = o[:id]
+        m[:organization_id] = 'legislature'
+      end
+    end
+  end
+
 end
 
 
