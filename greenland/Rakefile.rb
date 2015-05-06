c3c4cec0dea9e668a2709e32bc692ef72b4ed948
Tony Bowden <tony@tmtm.com>
2015-05-06 23:36:24 +0100

    Switch Greenland to new style Rakefile
    no output changes at all.

--- greenland/Rakefile.rb
+++ greenland/Rakefile.rb
@@ -6,18 +6,20 @@ require_relative '../rakefile_popit.rb'
   name: 'Inatsisartut',
 }
 
-task :add_term_dates => :load_json do
-  parl = @json[:organizations].find { |h| h[:classification] == 'legislature' } or raise "No legislature"
-  terms = parl[:legislative_periods].sort_by { |p| p[:name] }
-  terms.each_with_index do |p, i|
-    p[:start_date] = p[:name]
-    p[:name] = "Inatsisartut #{i+1}"
-    unless (i+1 == terms.size)
-      p[:end_date] = Date.parse(terms[i+1][:name]) - 1
+namespace :transform do
+
+  task :write => :fix_term_dates
+  task :fix_term_dates => :ensure_term do
+    parl = @json[:organizations].find { |h| h[:classification] == 'legislature' } or raise "No legislature"
+    terms = parl[:legislative_periods].sort_by { |p| p[:name] }
+    terms.each_with_index do |p, i|
+      p[:start_date] = p[:name]
+      p[:name] = "Inatsisartut #{i+1}"
+      unless (i+1 == terms.size)
+        p[:end_date] = Date.parse(terms[i+1][:name]) - 1
+      end
     end
   end
+
 end
-
-task :process_json => :add_term_dates
-
 
