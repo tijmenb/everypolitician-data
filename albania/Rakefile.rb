af7fd515f49cc8ab9088918869ea288b3398399d
Tony Bowden <tony@tmtm.com>
2015-05-07 21:24:34 +0100

    on_behalf_of (take 1) for Albania
    (and refreshed data, in case some of the duplicate party info has been
    fixed at source)

--- albania/Rakefile.rb
+++ albania/Rakefile.rb
@@ -6,3 +6,10 @@ require_relative '../rakefile_parldata.rb'
   name: 'Kuvendi'
 }
 @FACTION_CLASSIFICATION = 'parliamentary_group'
+
+namespace :whittle do
+
+  task :standardise_terminology do
+    # binding.pry
+  end
+end
