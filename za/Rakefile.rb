a6a9c7ac917800236a1edaabd241c2e14aba1fcf
Tony Bowden <tony@tmtm.com>
2015-05-09 08:11:50 +0100

    Only have one legislature
    Remove the National Council of Provinces, and standardise
    classifications.

--- za/Rakefile.rb
+++ za/Rakefile.rb
@@ -31,26 +31,30 @@ namespace :whittle do
 
   task :no_orphaned_memberships => :clean_orphaned_people
     
-  #TODO: remove Parliaments that aren't the National Assembly
   task :delete_unwanted_orgs => :load do
-    # puts "Starting with #{@json[:organizations].count} orgs"
+    puts "Starting with #{@json[:organizations].count} orgs"
     keep = @json[:organizations].find_all { |o| @KEEP_ORG_TYPES.include? o[:classification].downcase }
     keepids = keep.map { |o| o[:id] }
     @json[:memberships].keep_if   { |m| keepids.include? m[:organization_id] }
-    @json[:organizations].keep_if { |m| keepids.include? m[:id] }
-    # puts "Ending with #{@json[:organizations].count} orgs"
+    @json[:organizations].keep_if { |o| keepids.include? o[:id] }
+    @json[:organizations].each do |o| 
+      o[:classification].downcase!
+      o[:classification] = 'legislature' if o[:classification] == 'parliament'
+    end
+    @json[:organizations].delete_if { |o| o[:classification] == 'legislature' and o[:name] != 'National Assembly' }
+    puts "Ending with #{@json[:organizations].count} orgs"
   end
 
   # Delete everyone whose only Membership is to a Party
   task :clean_orphaned_people => :delete_unwanted_orgs do
-    # puts "Starting with #{@json[:persons].count} people"
-    orgt = Hash[@json[:organizations].map { |o| [ o[:id], o[:classification].downcase] } ]
+    puts "Starting with #{@json[:persons].count} people"
+    orgt = Hash[@json[:organizations].map { |o| [ o[:id], o[:classification] ] } ]
     @json[:persons].delete_if { |p| 
       @json[:memberships].find_all { |m| 
         m[:person_id] == p[:id] and orgt[ m[:organization_id] ]!= 'party' 
       }.count.zero?
     }
-    # puts "Ending with #{@json[:persons].count} people"
+    puts "Ending with #{@json[:persons].count} people"
   end
 
   task :write => :remove_interest_register
