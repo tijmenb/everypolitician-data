cf052670c8d0e20acc991f1694fd021be2899af5
Tony Bowden <tony@tmtm.com>
2015-05-09 09:44:57 +0100

    Add onBehalfOf (take 1)
    Doesn’t cope yet with switching party

--- za/Rakefile.rb
+++ za/Rakefile.rb
@@ -66,7 +66,7 @@ end
 
 namespace :transform do
 
-  task :write => :add_legislative_periods_to_memberships
+  task :write => :fill_behalfs
 
   task :add_legislative_periods_to_memberships => :add_term_dates do
     leg  = @json[:organizations].find     { |h| h[:classification] == 'legislature' }
@@ -86,6 +86,46 @@ namespace :transform do
         missing[:legislative_period_id] = overlapping_terms.first[:id]
       else
         warn "No possible terms for #{missing}" 
+      end
+    end
+  end
+
+  task :fill_behalfs => :add_legislative_periods_to_memberships do
+    leg     = @json[:organizations].find     { |h| h[:classification] == 'legislature' }
+    parties = @json[:organizations].find_all { |h| h[:classification] == 'party' }
+    partyids = parties.map { |p| p[:id] }.to_set
+    terms    = leg[:legislative_periods]
+
+    # All Memberships that have no :on_behalf_of
+    gaps = @json[:memberships].find_all { |m| 
+        m.has_key?(:legislative_period_id) and not m.has_key?(:on_behalf_of_id)
+    }
+
+    gaps.each do |missing|
+      # What else was that Person a Member of during that Term?
+      term = terms.find { |t| t[:id] == missing[:legislative_period_id] }
+      possibles = @json[:memberships].find_all { |m| 
+        m[:person_id] == missing[:person_id] and m[:organization_id] != leg[:id]
+      }.reject { |pmem|
+        term[:end_date] and pmem[:start_date] and pmem[:start_date] > term[:end_date]
+      }.reject { |pmem|
+        term[:start_date] and pmem[:end_date] and pmem[:end_date] < term[:start_date]
+      }
+
+      party_mems = possibles.find_all { |m| partyids.include? m[:organization_id] }
+
+      # No factions, but single party? That'll do
+      if party_mems.count == 1
+        missing[:on_behalf_of_id] = party_mems.first[:organization_id]
+
+      # Multiple parties. Again, first for now, but TODO
+      elsif party_mems.count > 1
+        warn "Person #{missing[:person_id]} in multiple parties during Term #{term[:id]}"
+        missing[:on_behalf_of_id] = party_mems.first[:organization_id]
+
+      # None? class as Independent
+      else
+        warn "Person #{missing[:person_id]} in no parties during Term #{term[:id]}"
       end
     end
   end
