a2f74305a91c820d40d63ad76a8d64243ecddfd4
Tony Bowden <tony@tmtm.com>
2015-05-10 08:07:19 +0100

    transform the Danish memberships
    Pull this out of rakefile_common as it’s the only place using it.

--- denmark/Rakefile.rb
+++ denmark/Rakefile.rb
@@ -22,9 +22,6 @@ end
 
 namespace :transform do
 
-  # safe to trigger the 'simple' behalf_of switcher
-  task :ensure_membership_terms => :ensure_behalf_of
-
   task :write => :add_party_names
   task :add_party_names => :load do
     # from https://en.wikipedia.org/wiki/List_of_members_of_the_Folketing
@@ -46,6 +43,19 @@ namespace :transform do
         }]
         o[:name] = fullname
       end 
+    end
+  end
+
+  # This helper only works because the only other Orgs are the Parties 
+  # But make sure it's run before the default version
+  task :ensure_membership_terms => :create_on_behalf_ofs
+  task :create_on_behalf_ofs => :ensure_legislature do
+    @json[:organizations].find_all { |h| h[:classification] != 'legislature' }.each do |o|
+      @json[:memberships].find_all { |m| m[:organization_id] == o[:id] }.each do |m|
+        m[:role] = 'member'
+        m[:on_behalf_of_id] = o[:id]
+        m[:organization_id] = 'legislature'
+      end
     end
   end
 
