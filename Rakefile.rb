63e49a64d1a20c2e0e947322a9151c6bb55e7870
Tony Bowden <tony@tmtm.com>
2015-05-10 19:05:34 +0100

    make `rake country name` aware of new locations

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,28 +1,32 @@
 require 'tmpdir'
 
-@COUNTRIES = FileList['data/*/Rakefile.rb'].pathmap('%d')
+@COUNTRIES = FileList['data/*/Rakefile.rb'].map { |c| {
+  path: c.pathmap('%d'),
+  name: c.pathmap('%d').split('/').last,
+}}
 
 @COUNTRIES.each do |country|
-  desc "Regenerate #{country}"
-  task country.to_sym do 
+  desc "Regenerate #{country[:name]}"
+  task country[:name].to_sym do 
     Rake::Task[:regenerate].execute(country: country) 
   end
 end
 
 task :regenerate, :country do |t, args|
   country = args[:country] or abort "Need a country"
-  abort "Don't know how to build #{country}" unless @COUNTRIES.include? country
-  chdir country
-  sh 'rake rebuild'
-  chdir '..'
+  Dir.chdir country[:path] do
+    sh 'rake rebuild'
+  end
 end
 
 desc "Regenarate all countries"
 task :regenerate_all do
   @COUNTRIES.each do |country| 
-    Rake::Task[country.to_sym].execute
+    Rake::Task[country[:name].to_sym].execute
   end
 end
+
+
 
 desc "Publish data"
 task :publish do
