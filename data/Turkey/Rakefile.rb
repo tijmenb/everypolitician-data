8da7b66c5a659173af1bda6630b69db8e61be6ac
Tony Bowden <tony@tmtm.com>
2015-05-13 20:38:33 +0100

    Load the Terms from a CSV file
    move the Term data out of the Rakefile into a local CSV

--- data/Turkey/Rakefile.rb
+++ data/Turkey/Rakefile.rb
@@ -6,35 +6,11 @@ require_relative '../../rakefile_morph.rb'
   name: 'Grand National Assembly'
 }
 
-# From https://tr.wikipedia.org/wiki/T%C3%BCrkiye_B%C3%BCy%C3%BCk_Millet_Meclisi
-@TERMS = [
-  { 
-    id: 'term/24',
-    name: '24th Parliament',
-    start_date: '2011-06-28',
-  },
-  { 
-    id: 'term/23',
-    name: '23rd Parliament',
-    start_date: '2007-07-23',
-    end_date: '2011-06-28',
-  },
-  { 
-    id: 'term/22',
-    name: '22nd Parliament',
-    start_date: '2002-11-14',
-    end_date: '2007-07-23',
-  },
-  { 
-    id: 'term/21',
-    name: '21st Parliament',
-    start_date: '1999-04-18',
-    end_date: '2002-11-14',
-  },
-  { 
-    id: 'term/20',
-    name: '20th Parliament',
-    start_date: '1996-01-08',
-    end_date: '1999-04-18',
-  },
-]
+@TERMS = CSV.read("terms.csv", headers:true).map do |row|
+  {
+    id: "term/#{row['id']}",
+    name: row['name'],
+    start_date: row['start_date'],
+    end_date: row['end_date'],
+  }.reject { |_,v| v.nil? or v.empty? }
+end
