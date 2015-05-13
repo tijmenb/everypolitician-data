97f38c1528e708665ab4b0e41f2b06300645e537
Tony Bowden <tony@tmtm.com>
2015-05-13 21:03:11 +0100

    Mongolia: add termfile

--- data/Mongolia/Rakefile.rb
+++ data/Mongolia/Rakefile.rb
@@ -6,18 +6,3 @@ require_relative '../../rakefile_morph.rb'
   name: 'State Great Khural'
 }
 
-# From https://tr.wikipedia.org/wiki/T%C3%BCrkiye_B%C3%BCy%C3%BCk_Millet_Meclisi
-@TERMS = [
-  { 
-    id: 'term/2012',
-    name: 'State Grand Khural 2012–',
-    start_date: '2012-06-28',
-  },
-  { 
-    id: 'term/2008',
-    name: 'State Grand Khural 2008–2012',
-    start_date: '2008-07-23',
-    end_date: '2012-06-28',
-  },
-]
-
