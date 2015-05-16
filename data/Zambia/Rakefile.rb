e40698b247e9342b2c74c427b8853dcffef305cf
Tony Bowden <tony@tmtm.com>
2015-05-16 13:41:52 +0100

    Zambia: Add term details

--- data/Zambia/Rakefile.rb
+++ data/Zambia/Rakefile.rb
@@ -7,7 +7,7 @@ require_relative '../../rakefile_morph.rb'
 
 # http://www.parliament.gov.zm/publications/debates-proceedings/page/6/0
 @TERMS = [{
-  id: 2011,
+  id: 'term/2011',
   name: "11th Assembly",
   start_date: '2011-10-06',
 }]
