e9763909e23c914e6411655ffe7092f5408d233e
Tony Bowden <tony@tmtm.com>
2015-05-16 13:40:51 +0100

    Zambia: Initial data

---
+++ data/Zambia/Rakefile.rb
@@ -0,0 +1,13 @@
+require_relative '../../rakefile_morph.rb'
+
+@MORPH = 'tmtmtmtm/zambia-na-scraper'
+@LEGISLATURE = {
+  name: 'National Assembly'
+}
+
+# http://www.parliament.gov.zm/publications/debates-proceedings/page/6/0
+@TERMS = [{
+  id: 2011,
+  name: "11th Assembly",
+  start_date: '2011-10-06',
+}]
