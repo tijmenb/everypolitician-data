3ef4d40b89b265673f6cad98560adc491db4386b
Tony Bowden <tony@tmtm.com>
2015-05-26 21:00:26 +0100

    Pull in terms table

--- data/Ghana/Rakefile.rb
+++ data/Ghana/Rakefile.rb
@@ -5,3 +5,15 @@ require_relative '../../rakefile_morph.rb'
   name: 'Parliament',
   seats: 228,
 }
+
+namespace :raw do
+
+  @DEFAULT_MORPH_TERM_QUERY = 'SELECT * FROM terms'
+  file 'morph.csv' => 'terms.csv'
+
+  file 'terms.csv' do
+    File.write('terms.csv', morph_select(@MORPH_TERM_QUERY || @DEFAULT_MORPH_TERM_QUERY))
+  end
+
+end
+
