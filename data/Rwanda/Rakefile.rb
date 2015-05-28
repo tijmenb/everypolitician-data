557c48f0ce7ae03cc74e05e58ac13a27f78664f3
Tony Bowden <tony@tmtm.com>
2015-05-28 16:03:47 +0300

    Rwanda: Initial data

---
+++ data/Rwanda/Rakefile.rb
@@ -0,0 +1,13 @@
+require_relative '../../rakefile_morph.rb'
+
+@MORPH = 'tmtmtmtm/rwanda-chamber-of-deputies'
+@LEGISLATURE = {
+  name: 'Chamber of Deputies',
+  seats: 80,
+}
+
+@TERMS = [{
+  id: 'term/2013',
+  name: '3rd Legislature',
+  start_date: '2013'
+}]
