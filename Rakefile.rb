47d7846cf068b90e4a2d0e058396ac6b946089ca
Tony Bowden <tony@tmtm.com>
2015-06-04 15:18:42 +0100

    Add ISO country codes to the countries.json
    For now just copy the code from viewer-sinatra

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,5 +1,7 @@
-require 'tmpdir'
 require 'json'
+require 'iso_country_codes'
+require 'tmpdir'
+
 
 @COUNTRIES = FileList['data/*/Rakefile.rb'].map { |c| {
   path: c.pathmap('%d'),
@@ -28,14 +30,40 @@ task :regenerate_all do
   end
 end
 
+
+ISO = IsoCountryCodes.for_select
+ISO_REMAP = { 
+  'Congo-Brazzaville' => 'CG',
+  'Kosovo' => 'XK',
+  'UK' => 'GB',
+  'Scotland' => 'GB-SCT',
+  'Wales' => 'GB-WLS',
+  'Northern Ireland' => 'GB-NIR',
+}
+
+def name_to_iso_code(name)
+  if code = ISO_REMAP[name]
+    return code
+  elsif code = ISO.find { |iname, _| iname == name }
+    return code.last
+  elsif code = ISO.find { |iname, _| iname.start_with? name }
+    return code.last
+  else 
+    raise "Can't find country code for #{name}"
+  end
+end
+
+
 desc "Install country-list locally"
 task 'countries.json' do 
   data = @COUNTRIES.reject { |c| File.exist? "#{c[:path]}/WIP" }.map do |country| 
+    name = country[:name].tr('_', ' ')
     popolo = country[:path] + '/final.json'
     cmd = "git log -p --format='%h|%at' --no-notes -s -1 #{popolo}"
     (sha, lastmod) = %x(#{cmd}).chomp.split('|')
     {
-      country: country[:name],
+      country: name,
+      code: name_to_iso_code(name),
       popolo: popolo,
       lastmod: lastmod,
       sha: sha,
