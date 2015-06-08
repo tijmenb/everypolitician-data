5f6dce1b7572e916145f6d2e6c916dd0997360e1
Tony Bowden <tony@tmtm.com>
2015-06-08 11:17:56 +0100

    Angola: Split out terms.csv

--- data/Angola/Rakefile.rb
+++ data/Angola/Rakefile.rb
@@ -5,9 +5,3 @@ require_relative '../../rakefile_morph.rb'
   name: 'National Assembly',
   seats: 220,
 }
-
-@TERMS = [{
-  id: 'term/3',
-  name: "3rd National Assembly",
-  start_date: '2012',
-}]
