4d855473ac7a15a5ea4ef7528677346b171ed3d9
Tony Bowden <tony@tmtm.com>
2015-06-11 15:19:11 +0100

    New parlparse-data Rakefile

---
+++ data/UK/Rakefile.rb
@@ -0,0 +1,13 @@
+require_relative '../../rakefile_parlparse.rb'
+
+@LEGISLATURE = {
+  name: 'House of Commons',
+  seats: 650,
+}
+
+namespace :whittle do
+  task :load do
+    @json[:memberships].delete_if { |m| m.key?(:start_date) && m[:start_date][0..3] < '1801' }
+  end
+end
+
