742128d016825df793bb32d1739c00ded9de24c0
Tony Bowden <tony@tmtm.com>
2015-06-12 08:53:47 +0100

    Transform `details_url` into `source`

--- data/Monaco/Rakefile.rb
+++ data/Monaco/Rakefile.rb
@@ -1,7 +1,7 @@
 require_relative '../../rakefile_morph.rb'
 
 @MORPH = 'duncanparkes/monaco'
-@MORPH_QUERY = 'SELECT *, term_id AS term FROM data'
+@MORPH_QUERY = 'SELECT id, name, party, area, image, email, term_id AS term, details_url AS source FROM data'
 @MORPH_TERMS = true
 @LEGISLATURE = {
   name: 'National Council',
