5919a92cdfc14e2530cddcb45ab9ffdc676b6c42
Tony Bowden <tony@tmtm.com>
2015-06-12 13:11:56 +0100

    Australia: add party_id

--- data/Australia/Rakefile.rb
+++ data/Australia/Rakefile.rb
@@ -1,6 +1,7 @@
 require_relative '../../rakefile_morph.rb'
 
 @MORPH = 'openaustralia/aus_mp_contact_details'
+@MORPH_QUERY = "SELECT aph_id AS id, full_name AS name, electorate AS constituency, photo_url AS photo, REPLACE(LOWER(party),' ','_') AS party_id, * FROM data"
 
 @LEGISLATURE = {
   name: 'Parliament',
@@ -15,8 +16,6 @@ require_relative '../../rakefile_morph.rb'
   name: '44th Parliament',
   start_date: '2013-09-07',
 }]
-
-@MORPH_QUERY = 'SELECT aph_id AS id, full_name AS name, electorate AS constituency, photo_url AS photo, * FROM data'
 
 namespace :transform do
 
