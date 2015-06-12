5d36dadb15c9a2a4c4175250ba3d4de9736045ca
Tony Bowden <tony@tmtm.com>
2015-06-12 08:30:19 +0100

    Monaco: Fetch term_id as term
    The CSV importer doesn’t like it being called `term_id`

--- data/Monaco/Rakefile.rb
+++ data/Monaco/Rakefile.rb
@@ -1,6 +1,7 @@
 require_relative '../../rakefile_morph.rb'
 
 @MORPH = 'duncanparkes/monaco'
+@MORPH_QUERY = 'SELECT *, term_id AS term FROM data'
 @MORPH_TERMS = true
 @LEGISLATURE = {
   name: 'National Council',
