19695b1e80a3649091658e861a43ae7f347f733b
Tony Bowden <tony@tmtm.com>
2015-06-19 16:48:22 +0100

    San Marino: Initial data

---
+++ data/San_Marino/Council/Rakefile.rb
@@ -0,0 +1,9 @@
+require_relative '../../../rakefile_morph.rb'
+
+@MORPH = 'tmtmtmtm/san_marino_council'
+@MORPH_TERMS = true
+@LEGISLATURE = {
+  name: 'Grand and General Council',
+  seats: 60,
+}
+
