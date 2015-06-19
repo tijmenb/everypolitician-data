d72f6d9a13b51bf20209ec4f9f0cbc85bda7b194
Tony Bowden <tony@tmtm.com>
2015-06-19 10:35:08 +0100

    Jersey: Add areas

--- data/Jersey/Rakefile.rb
+++ data/Jersey/Rakefile.rb
@@ -1,6 +1,7 @@
 require_relative '../../rakefile_morph.rb'
 
 @MORPH = 'tmtmtmtm/jersey-assembly'
+@MORPH_QUERY = 'SELECT *, parish AS area FROM data'
 @MORPH_TERMS = true
 @LEGISLATURE = {
   name: 'States',
