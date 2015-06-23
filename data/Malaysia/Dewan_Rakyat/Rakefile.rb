d46e08c7e95f49564b0dcd4ab4656fe21957d543
Tony Bowden <tony@tmtm.com>
2015-06-23 10:40:54 +0100

    Malaysia: more precise start date for current term
    and drop the source info from each person for now, to make it easier to
    read

--- data/Malaysia/Dewan_Rakyat/Rakefile.rb
+++ data/Malaysia/Dewan_Rakyat/Rakefile.rb
@@ -1,6 +1,7 @@
 require_relative '../../../rakefile_morph.rb'
 
 @MORPH = 'tmtmtmtm/malaysian_parliament-wp'
+@MORPH_QUERY = "SELECT *, '' AS source FROM data"
 @LEGISLATURE = {
   name: 'Dewan Rakyat',
   seats: 222,
