043627422415e18f4a143b375c6882baaa7ea50b
Tony Bowden <tony@tmtm.com>
2015-06-30 22:03:55 +0100

    Burundi: move term information to terms.csv

--- data/Burundi/Assembly/Rakefile.rb
+++ data/Burundi/Assembly/Rakefile.rb
@@ -5,10 +5,3 @@ require_relative '../../../rakefile_morph.rb'
   name: 'Assemblée nationale',
   seats: 118,
 }
-
-@TERMS = [{ 
-  id: "term/2010",
-  name: "2010",
-  start_date: "2010",
-  end_date: '2015-04-30',
-}]
