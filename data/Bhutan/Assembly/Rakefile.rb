a95e33dbd13fcfd4b4fd5716fd300969712b4455
Tony Bowden <tony@tmtm.com>
2015-06-30 22:02:07 +0100

    Bhutan: move term information to terms.csv

--- data/Bhutan/Assembly/Rakefile.rb
+++ data/Bhutan/Assembly/Rakefile.rb
@@ -5,18 +5,3 @@ require_relative '../../../rakefile_morph.rb'
   name: 'National Assembly',
   seats: 47,
 }
-
-# http://www.nab.gov.bt/about/parliament-session
-@TERMS = [
-  {
-    id: 'term/1',
-    name: "1st Parliament",
-    start_date: '2008-03-01',
-    end_date: '2013-04-30',
-  },
-  {
-    id: 'term/2',
-    name: "2nd Parliament",
-    start_date: '2013-07-13',
-  },
-]
