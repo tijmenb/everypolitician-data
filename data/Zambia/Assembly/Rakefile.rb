74b6edd7f899f77c11551cd5f245b75593a56222
Tony Bowden <tony@tmtm.com>
2015-06-30 22:20:06 +0100

    Zambia: move term information to terms.csv

--- data/Zambia/Assembly/Rakefile.rb
+++ data/Zambia/Assembly/Rakefile.rb
@@ -5,10 +5,3 @@ require_relative '../../../rakefile_morph.rb'
   name: 'National Assembly',
   seats: 150,
 }
-
-# http://www.parliament.gov.zm/publications/debates-proceedings/page/6/0
-@TERMS = [{
-  id: 'term/2011',
-  name: "11th Assembly",
-  start_date: '2011-10-06',
-}]
