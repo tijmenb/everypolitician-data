d84d5dd7c8905f97bd1feaadf8dbf6f82c928f3b
Tony Bowden <tony@tmtm.com>
2015-07-01 11:09:24 +0100

    Dominican Republic: Move to instructions.json

--- data/Dominican_Republic/Diputados/Rakefile.rb
+++ data/Dominican_Republic/Diputados/Rakefile.rb
@@ -1,7 +1,5 @@
 require_relative '../../../rakefile_morph.rb'
 
-@MORPH = 'tmtmtmtm/dominican-republic-deputies'
-@MORPH_TERMS = true
 @LEGISLATURE = {
   name: 'Chamber of Deputies',
   seats: 190,
