ebc1fadda2896f40b2c8d1bd0388251817d1da4d
Tony Bowden <tony@tmtm.com>
2015-07-01 22:56:32 +0100

    Serbia: Add legislature meta.json

--- data/Serbia/Assembly/Rakefile.rb
+++ data/Serbia/Assembly/Rakefile.rb
@@ -2,10 +2,6 @@
 require_relative '../../../rakefile_parldata.rb'
 
 @PARLDATA = 'rs/skupstina'
-@LEGISLATURE = {
-  name: 'Skupština',
-  seats: 250,
-}
 @FACTION_CLASSIFICATION = 'parliamentary_group'
 @MEMBERSHIP_GROUPING = 'faction'
 
