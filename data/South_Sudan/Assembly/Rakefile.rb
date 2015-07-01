4f80601e0455f1261b98ce983b5083daa7930480
Tony Bowden <tony@tmtm.com>
2015-07-01 13:31:08 +0100

    South Sudan: Move to instructions.json

--- data/South_Sudan/Assembly/Rakefile.rb
+++ data/South_Sudan/Assembly/Rakefile.rb
@@ -1,7 +1,5 @@
 require_relative '../../../rakefile_morph.rb'
 
-@MORPH = 'tmtmtmtm/south-sudan-legislative-assembly'
-@MORPH_TERMS = true
 @LEGISLATURE = {
   name: 'National Legislative Assembly',
   seats: 170,
