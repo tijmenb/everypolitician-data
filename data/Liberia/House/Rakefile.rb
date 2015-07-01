8122891b92e8f7679db162390e08c4fbef5b6f09
Tony Bowden <tony@tmtm.com>
2015-07-01 12:38:16 +0100

    Liberia: Move to instructions.json

--- data/Liberia/House/Rakefile.rb
+++ data/Liberia/House/Rakefile.rb
@@ -1,7 +1,5 @@
 require_relative '../../../rakefile_morph.rb'
 
-@MORPH = 'tmtmtmtm/liberia-representatives'
-@MORPH_TERMS = true
 @LEGISLATURE = {
   name: 'House of Representatives',
   seats: 73,
