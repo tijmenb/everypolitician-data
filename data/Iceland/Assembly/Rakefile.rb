2de7f79ccf1f5a5a7635bb6de376b565ba5c822a
Tony Bowden <tony@tmtm.com>
2015-07-01 11:13:40 +0100

    Iceland: Move to instructions.json

--- data/Iceland/Assembly/Rakefile.rb
+++ data/Iceland/Assembly/Rakefile.rb
@@ -1,8 +1,5 @@
 require_relative '../../../rakefile_morph.rb'
 
-@MORPH = 'tmtmtmtm/iceland-althing-wp'
-@MORPH_QUERY = "SELECT REPLACE(LOWER(name),' ','_') AS id, REPLACE(LOWER(party),' ','_') AS party_id, * FROM data"
-
 @LEGISLATURE = {
   name: 'Alþingi',
   seats: 63,
