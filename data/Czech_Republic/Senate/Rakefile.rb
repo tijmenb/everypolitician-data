5dc0e75be78753166aeed6dbbe69c68bbf408d99
Tony Bowden <tony@tmtm.com>
2015-07-01 22:34:08 +0100

    Czech Senat: Add legislature meta.json

--- data/Czech_Republic/Senate/Rakefile.rb
+++ data/Czech_Republic/Senate/Rakefile.rb
@@ -1,10 +1,6 @@
 require_relative '../../../rakefile_parldata.rb'
 
 @PARLDATA = 'cz/senat' 
-@LEGISLATURE = {
-  name: 'Senate',
-  seats: 81,
-}
 @FACTION_CLASSIFICATION = 'political group'
 @MEMBERSHIP_GROUPING = 'faction'
 
