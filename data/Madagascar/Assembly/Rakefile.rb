36df4d164c897595c65ae027d103b0e13753ae43
Tony Bowden <tony@tmtm.com>
2015-07-01 12:40:53 +0100

    Madagascar: Move to instructions.json

--- data/Madagascar/Assembly/Rakefile.rb
+++ data/Madagascar/Assembly/Rakefile.rb
@@ -1,8 +1,5 @@
 require_relative '../../../rakefile_morph.rb'
 
-@MORPH = 'duncanparkes/madagascar'
-@MORPH_TERMS = true
-# @MORPH_QUERY = "SELECT *, area AS constituency FROM data"
 @LEGISLATURE = {
   name: 'National Parliament',
   seats: 160
