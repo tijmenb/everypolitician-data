56289ba12db215001ac88d47355e84f4d566eb69
Tony Bowden <tony@tmtm.com>
2015-07-01 12:11:23 +0100

    Jersey: Move to instructions.json

--- data/Jersey/States/Rakefile.rb
+++ data/Jersey/States/Rakefile.rb
@@ -1,8 +1,5 @@
-require_relative '../../rakefile_morph.rb'
+require_relative '../../../rakefile_morph.rb'
 
-@MORPH = 'tmtmtmtm/jersey-assembly'
-@MORPH_QUERY = 'SELECT *, parish AS area FROM data'
-@MORPH_TERMS = true
 @LEGISLATURE = {
   name: 'States',
   seats: 39,
