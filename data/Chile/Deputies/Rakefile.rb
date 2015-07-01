6c52f925f4b55b05379f7cc80b123e7d047d6ad9
Tony Bowden <tony@tmtm.com>
2015-07-01 18:03:03 +0100

    Chile: replace the manual whittle task with better SQL
    We can get rid of the check for district = 0 by inlining it into the
    initial SQL

--- data/Chile/Deputies/Rakefile.rb
+++ data/Chile/Deputies/Rakefile.rb
@@ -1,14 +1,1 @@
 require_relative '../../../rakefile_morph.rb'
-
-namespace :whittle do
-
-  task :write => :clean_zero_districts
-
-  task :clean_zero_districts => :load do
-    @json[:memberships].each do |m|
-      m.delete(:area) if m.has_key?(:area) and m[:area][:name] == '0'
-    end
-  end
-end
-
-
