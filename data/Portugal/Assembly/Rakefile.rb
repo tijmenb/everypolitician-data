d74e78eae01237dc5aa04ce1d3275bc3f97a86b6
Tony Bowden <tony@tmtm.com>
2015-07-01 13:28:31 +0100

    Portugal: Move to instructions.json

--- data/Portugal/Assembly/Rakefile.rb
+++ data/Portugal/Assembly/Rakefile.rb
@@ -1,7 +1,5 @@
 require_relative '../../../rakefile_morph.rb'
 
-@MORPH = 'tmtmtmtm/portugal-democratica-scraper'
-@MORPH_TERMS = true
 @LEGISLATURE = {
   name: 'Assembleia da República',
   seats: 230,
