d8343febd759501aaed0b975a0331c4f7b82129a
Tony Bowden <tony@tmtm.com>
2015-07-01 13:32:45 +0100

    Tonga: Move to instructions.json

--- data/Tonga/Assembly/Rakefile.rb
+++ data/Tonga/Assembly/Rakefile.rb
@@ -1,8 +1,5 @@
 require_relative '../../../rakefile_morph.rb'
 
-@MORPH = 'duncanparkes/tonga'
-@MORPH_TERMS = true
-@MORPH_QUERY = "SELECT id, name, constituency, 'unknown' AS party, term_id AS term, image, email, phone, cell, details_url AS source FROM data"
 @LEGISLATURE = {
   name: 'Legislative Assembly',
   seats: 26
