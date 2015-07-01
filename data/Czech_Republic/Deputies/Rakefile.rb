5a45c3116bd796fdcfda7da20d3d3e5175d076c3
Tony Bowden <tony@tmtm.com>
2015-07-01 22:33:44 +0100

    Czech Deputies: Add legislature meta.json

--- data/Czech_Republic/Deputies/Rakefile.rb
+++ data/Czech_Republic/Deputies/Rakefile.rb
@@ -1,10 +1,6 @@
 require_relative '../../../rakefile_parldata.rb'
 
 @PARLDATA = 'cz/psp'
-@LEGISLATURE = {
-  name: 'Chamber of Deputies',
-  seats: 200,
-}
 @FACTION_CLASSIFICATION = 'political group'
 @MEMBERSHIP_GROUPING = 'faction'
 
