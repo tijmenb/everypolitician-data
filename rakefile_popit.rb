d3e87281974fc711771ffb4974f12fe2b8b300db
Tony Bowden <tony@tmtm.com>
2015-07-05 16:22:08 +0100

    Add a CLOBBER task for Popit-based Rakefiles

--- rakefile_popit.rb
+++ rakefile_popit.rb
@@ -4,13 +4,15 @@ require_relative 'rakefile_common.rb'
 desc "Reload the source data"
 task :raw => 'popit.json'
 
+
+@POPIT_RAW_FILE = 'sources/popit/raw.json'
+CLOBBER.include(@POPIT_RAW_FILE)
+
 def popit_source 
   @POPIT_URL || "https://#{@POPIT || @DEST}.popit.mysociety.org/api/v0.1/export.json"
 end
 
 namespace :raw do
-
-  @POPIT_RAW_FILE = 'sources/popit/raw.json'
 
   file @POPIT_RAW_FILE do
     File.write(@POPIT_RAW_FILE, open(popit_source).read) 
