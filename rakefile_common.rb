af384f477589acd51e765eacaaacb089e0c47d2e
Tony Bowden <tony@tmtm.com>
2015-09-06 21:37:26 +0100

    split out rakefile_whittle
    A separate Rakefile for the `:whittle` task, to generate
    `sources/merged.json`
    
    (Somewhat misnamed, but we’ll combine it with rakefile_merged soon)

--- rakefile_common.rb
+++ rakefile_common.rb
@@ -45,48 +45,6 @@ desc "Rebuild from source data"
 task :rebuild => [ :clobber, 'ep-popolo-v1.0.json' ]
 task :default => :csvs
 
-desc "Remove unwanted data from source"
-task :whittle => [:clobber, 'sources/merged.json']
-
-namespace :whittle do
-
-  # Source-specific files must provide a whittle:load
-
-  file 'sources/merged.json' => :write 
-  CLEAN.include('sources/merged.json')
-
-  # Source-specific files must provide a @SOURCE
-
-  task :meta_info => :load do
-    @json[:meta] ||= {}
-    @json[:meta][:source] = @SOURCE || instructions(:source) || abort("No @SOURCE defined")
-  end
-
-  # Remove any 'warnings' left behind from (e.g.) csv-to-popolo
-  task :write => :remove_warnings
-  task :remove_warnings => :load do
-    @json.delete :warnings
-  end
-
-  # TODO work out how to make this do the 'only run if needed'
-  task :write => :meta_info do
-    unless File.exists? 'sources/merged.json'
-      json_write('sources/merged.json', @json)
-    end
-  end
-
-  #---------------------------------------------------------------------
-  # Rule: No orphaned memberships
-  #---------------------------------------------------------------------
-  task :write => :no_orphaned_memberships
-  task :no_orphaned_memberships => :load do
-    @json[:memberships].keep_if { |m|
-      @json[:organizations].find { |o| o[:id] == m[:organization_id] } and
-      @json[:persons].find { |p| p[:id] == m[:person_id] } 
-    }
-  end  
-end
-
 #-----------------------------------------------------------------------
 # Transform the results from generic CSV-to-Popolo into EP-Popolo
 #
