80eda3748652346eff6badcc3f474fbe186ba64f
Tony Bowden <tony@tmtm.com>
2015-09-22 16:15:37 +0100

    Check that all dates are valid

--- rake_helpers/verify_source_data.rb
+++ rake_helpers/verify_source_data.rb
@@ -1,4 +1,9 @@
 
+# After generating the merged CSV, ensure that it contains what we need
+# and is well-formed
+#
+# We don't need to check the raw source data as it may be overridden.
+
 desc "Verify merged data"
 
 namespace :verify do
@@ -10,6 +15,13 @@ namespace :verify do
   task :check_data => :load do
     @csv.each do |r|
       abort "No `name` in #{r}" if r[:name].to_s.empty?
+
+      r.keys.select { |k| k.to_s.include? '_date' }.each do |d|
+        next if r[d].nil? || r[d].empty?
+        abort "Badly formatted #{d} in #{r}" unless r[d].match /^\d{4}-\d{2}-\d{2}$/
+        parsed_date = Date.parse(r[d]) rescue 'broken'
+        abort "Invalid #{d} in #{r}" unless parsed_date.to_s == r[d]
+      end
     end
   end  
 end
