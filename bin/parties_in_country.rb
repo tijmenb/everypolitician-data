f687f7437273e38d76e6905c1a3f35b90ecf0ae9
Tony Bowden <tony@tmtm.com>
2015-10-13 19:09:54 +0100

    Add from/to dates

--- bin/parties_in_country.rb
+++ bin/parties_in_country.rb
@@ -65,6 +65,21 @@ class Item
 end
 
 
+def snakdate(c)
+  v = c[:mainsnak][:datavalue][:value]
+  # https://www.wikidata.org/wiki/Special:ListDatatypes
+  if v[:precision] == 8  # decade
+    v[:time][1..3] + "0s"
+  elsif v[:precision] == 9  # year
+    v[:time][1..4]
+  elsif v[:precision] == 10  # month
+    v[:time][1..7]
+  elsif v[:precision] == 11  # day
+    v[:time][1..10]
+  else
+    abort "unknown precision in #{v}"
+  end
+end
 
 meta = json_from('meta.json')
 house = Item.new(meta[:wikidata])
@@ -95,12 +110,23 @@ party_ids = party_json[:items] or abort "No parties"
 
 puts "PARTIES".yellow
 
-party_ids.each do |pid|
+parties = party_ids.map { |pid|
   party = Item.new(pid)
-  puts party.url.to_s.magenta
-  puts party.label(:en)
-  # puts party.all_labels.join(" // ")
-end
+  data = { 
+    name__en: party.label(:en),
+    # labels: party.all_labels.join("  //  "),
+    url: party.url,
+  }
+  if dfrom = party.claim(571)
+    data[:start_date] = snakdate(dfrom)
+  end
+  if dto = party.claim(576)
+    data[:end_date] = snakdate(dto)
+  end
+  data
+}
+
+puts parties.sort_by { |p| [p[:end_date] || '9999-99-00', p[:start_date] || '0000-00-00'] }.reverse
 
 
 
